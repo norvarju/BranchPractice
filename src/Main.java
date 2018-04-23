427e0963c3fd87346280e4a0a5d8831107d86300
Nóra Varju <snailjokes@gmail.com>
2018-04-23 14:49:35 +0200

    a5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
   public static void main(String[] args) {
-    greet();
+    greet("Nóra", "Varju");
   }
 
-  public static void greet() {
-    System.out.println("Greetings!");
+  public static void greet(String firstName, String lastName) {
+    System.out.println(firstName);
+    System.out.println(lastName);    
   }
 }
