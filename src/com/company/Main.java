4b14cabadcd250c48ef5b4a994d23e95d939ec1a
balazs-varga <balazsvarga91@gmail.com>
2018-04-23 13:24:09 +0200

    b1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+  public static void main(String[] args) {
+
+    count();
+  }
+
+  public static void count() {
+    System.out.println("27");
+  }
+}
