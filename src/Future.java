c4138cc57f84970a5ee9540df36dd14cd68d2e6c
pataricza <pataricza1987@gmail.com>
2018-04-23 14:41:41 +0200

    d4

--- src/Future.java
+++ src/Future.java
@@ -1,11 +1,6 @@
 public class Future {
 
-<<<<<<< HEAD
   public void printHoroscope() {
     System.out.println("Sagittarius");
-=======
-  public void printHoroscope(String printThis) {
-    System.out.println(printThis);
->>>>>>> 3f43251bbca469a1fac606eebc5bb0adafb31299
   }
 }
